2b24153d7aeaf9e680c87429304bb03483c2be1c
achopper <achopper@student.21-school.ru>
2022-01-11 21:12:55 +0300

    add copy and asign constructors

--- main.cpp
+++ main.cpp
@@ -10,8 +10,28 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "vector/Vector.hpp"
+
+class A
+{
+public:
+	int a;
+	A() : a(5) {}
+};
+
+class B
+{
+public:
+	int c;
+	B(int x) : c(x) {}
+};
 
 int main()
 {
+	A a;
+	A b;
+	ft::vector<A> myVec(10);
+	ft::vector<A> myVec1(20);
+	myVec = myVec1;
 	return (0);
 }
